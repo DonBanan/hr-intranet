6b602351b8145deb62d69998d138d1aa87a7a268
Alex Karahanidi <coder.django@gmail.com>
2022-06-03 12:40:09 +0600

    added history records template

--- apps/urls.py
+++ apps/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import IndexView
+from .views import IndexView, HistoricalRecordsList
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
+    path('historical-records/', HistoricalRecordsList.as_view(), name='historical-records'),
     path('', include('apps.accounts.urls')),
+    path('', include('apps.knowledge.urls')),
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class IndexView(TemplateView):
@@ -7,3 +8,12 @@ class IndexView(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super(IndexView, self).get_context_data(*args, **kwargs)
         return context
+
+
+class HistoricalRecordsList(LoginRequiredMixin, ListView):
+    paginate_by = 20
+    context_object_name = "records"
+    template_name = "historical_records_list.html"
+
+    def get_queryset(self):
+        return self.request.user.history.all()
